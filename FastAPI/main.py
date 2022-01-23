98ad4f203f5b6497258f9c17eb12debe89752070
Tigran Harutyunyan <harutyunyan.tigran.v@gmail.com>
2022-01-22 22:07:30 -0500

    Update main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -13,10 +13,7 @@ def root(req: Request):
     from_date = query_params['from_date']
     to_date = query_params['to_date']
     r = requests.get('https://api.coingecko.com/api/v3/coins/{}/market_chart/range?vs_currency={}&from={}&to={}'.format(id,currency,from_date,to_date ))
-    try:
-        return json.dumps({'Bitcoint_price': r.json()['prices']})
-    except Exception:
-        return Response("Internal server error", status_code=500)
+    return json.dumps({'Bitcoint_price': r.json()['prices']})
 
 if __name__ == "__main__":
     uvicorn.run(application, port=8080, host='0.0.0.0')
